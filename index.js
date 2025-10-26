14a5ee440cc44514e840ae87dda2ffdba215b834
Steven Lau <github@stevenlau.net>
2025-10-26 16:25:07 +0000

    Add Hotel Villa Favorita

--- index.js
+++ index.js
@@ -3,12 +3,12 @@
 function initMap() {
     const home = { lat: 51.41440159227802, lng: -0.007830720535929274 };
 
-    const martinb = { lat: 43.26694601928382, lng: -2.0156468200988975};
-
     const map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 10,
+        zoom: 5,
         center: home,
     });
+
+    //
     const homeContentString =
         '<div id="content">' +
         '<div id="siteNotice">' +
@@ -31,6 +31,37 @@ function initMap() {
         });
     });
 
+    // San Sebastian October 2025
+    const villaFavorita = { lat: 43.31700809564674, lng: -1.9858997367605542 };
+
+    const villaFavoritaContentString =
+        '<div id="content">' +
+        '<div id="siteNotice">' +
+        "</div>" +
+        '<h1 id="firstHeading" class="firstHeading">Hotel Villa Favorita</h1>' +
+        '<div id="bodyContent">' +
+        "The stunning hotel we stayed in on 19-20 October 2025" +
+        " that Kathryn organised for Steven's 50th birthday" +
+        "</div>" +
+        "</div>";
+
+    const villaFavoritaInfoWindow = new google.maps.InfoWindow({
+        content: villaFavoritaContentString,
+        ariaLabel: "Hotel Villa Favorita",
+    });
+    const villaFavoritaMarker = new google.maps.Marker({
+        position: villaFavorita,
+        map,
+        title: "Hotel Villa Favorita",
+    });
+    villaFavoritaMarker.addListener("click", () => {
+        villaFavoritaInfoWindow.open({
+            anchor: villaFavoritaMarker,
+            map,
+        });
+    });
+
+    const martinb = { lat: 43.26694601928382, lng: -2.0156468200988975 };
     const martinbContentString =
         '<div id="content">' +
         '<div id="siteNotice">' +
@@ -44,12 +75,12 @@ function initMap() {
 
     const martinbInfoWindow = new google.maps.InfoWindow({
         content: martinbContentString,
-        ariaLabel: "Martin B",
+        ariaLabel: "Martin Berasategui",
     });
     const martinbMarker = new google.maps.Marker({
         position: martinb,
         map,
-        title: "Martin B",
+        title: "Martin Berasategui",
     });
     martinbMarker.addListener("click", () => {
         martinbInfoWindow.open({
